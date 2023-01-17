eb8943972cbb3f238cadb715a628c7bbf3429a7f
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-17 11:53:31 +0900

    Initial Commit

---
+++ CopyTextToClipboard/CopyTextToClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyTextToClipboardApp.swift
+//  CopyTextToClipboard
+//
+//  Created by Seungchul Ha on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct CopyTextToClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
